0fdd4d6ed262314de714b55fd421fd7fa535d46b
ThulaniDissanayake <IT22157232@my.sliit.lk>
2025-03-28 19:30:50 +0530

    added update order  route

--- backend/order-management-service/routes/orderRoutes.js
+++ backend/order-management-service/routes/orderRoutes.js
@@ -4,7 +4,8 @@ import {
     getOrders,
     getOrderById,
     updateOrderStatus,
-    cancelOrder
+    cancelOrder,
+    updateOrderDetails
 } from "../controllers/orderController.js";
 
 import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
@@ -17,6 +18,9 @@ router.post("/", protect, authorizeRoles("customer"), createOrder);
 // Only restaurant admins & customers can view orders
 router.get("/", protect, authorizeRoles("customer", "restaurant"), getOrders);
 router.get("/:id", protect, authorizeRoles("customer", "restaurant"), getOrderById);
+
+// Only authenticated customers can update their own orders
+router.patch("/:id", protect, updateOrderDetails);
 
 // Only restaurant admins can update order status
 router.patch("/:id", protect, authorizeRoles("restaurant"), updateOrderStatus);
