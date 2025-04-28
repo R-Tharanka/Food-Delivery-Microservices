85fb71b2387977f9947d737f1c5858cf58122858
isiniperera <IT22250124@my.sliit.lk>
2025-04-28 12:24:57 +0530

    .

--- delivery-service/backend/src/middleware/authMiddleware.js
+++ delivery-service/backend/src/middleware/authMiddleware.js
@@ -5,14 +5,15 @@ dotenv.config();
 
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization");
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.driver = decoded.id;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+    res.status(401).json({ message: "Token is not valid" });
   }
 };
 
